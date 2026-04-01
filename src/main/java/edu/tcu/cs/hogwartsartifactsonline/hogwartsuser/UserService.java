f117cff3b81d46b43e26b800d40d8b6b9ddb7a0b
msthill <micah.sthill123@gmail.com>
2026-03-31 23:57:51 -0500

    authentication and authorization Close #5

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/UserService.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/UserService.java
@@ -3,19 +3,26 @@ package edu.tcu.cs.hogwartsartifactsonline.hogwartsuser;
 
 import edu.tcu.cs.hogwartsartifactsonline.system.exception.ObjectNotFoundException;
 import jakarta.transaction.Transactional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @Transactional
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
+    private PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public List<HogwartsUser> findAll() {
@@ -29,9 +36,17 @@ public class UserService {
 
     public HogwartsUser save(HogwartsUser newHogwartsUser) {
         // We NEED to encode plain password before saving to the DB! TODO
+        newHogwartsUser.setPassword(passwordEncoder.encode(newHogwartsUser.getPassword()));
         return this.userRepository.save(newHogwartsUser);
     }
 
+    /**
+     * We are not using this update to change user password.
+     *
+     * @param userId
+     * @param update
+     * @return
+     */
     public HogwartsUser update(Integer userId, HogwartsUser update) {
         HogwartsUser oldHogwartsUser = this.userRepository.findById(userId)
                 .orElseThrow(() -> new ObjectNotFoundException("user", userId));
@@ -47,4 +62,10 @@ public class UserService {
         this.userRepository.deleteById(userId);
     }
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByUsername(username)
+                .map(hogwartsUser -> new MyUserPrincipal(hogwartsUser))
+                .orElseThrow(() -> new UsernameNotFoundException("username " + username + " is not found."));
+    }
 }
